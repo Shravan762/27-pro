ac6b93d43680afe4af127a5da46f9a1974297c02
Shravan762 <75240159+Shravan762@users.noreply.github.com>
2021-05-02 00:16:52 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,7 +5,8 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
 const Constraint = Matter.Constraint;
-var bobObject1,bobObject2,bobObject3, bobObject4,bobObject5, roofObject
+//var bobObject1,bobObject2,bobObject3, bobObject4,bobObject5, roofObject
+var bob1,bob2,bob3,bob4,bob5
 var rope1,rope2,rope3, rope4,rope5;
 var world;
 
@@ -18,17 +19,18 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	roofObject=new roof(400,250,230,20);
-	bob1 = new bob(320,575,40)
-	bob2 = new bob(360,575,40)
-	bob3 = new bob(400,575,40)
-	bob4 = new bob(440,575,40)
-	bob5 = new bob(480,575,40)
-	rope1=new rope(bob1.body,roofObject.body,-80, 0)
-	rope2=new rope(bob2.body,roofObject.body,-40, 0)
-	rope3=new rope(bob3.body,roofObject.body,-0,0)
-	rope4=new rope(bob4.body,roofObject.body,40,0)
-    rope5=new rope(bob5.body,roofObject.body,80,0)
+	roof=new Roof(400,250,230,20);
+	bob1 = new Bob(320,575,40)
+	bob2 = new Bob(360,575,40)
+	bob3 = new Bob(400,575,40)
+	bob4 = new Bob(440,575,40)
+	bob5 = new Bob(480,575,40)
+	rope1=new Rope(bob1.body,roof.body,-80, 0)
+	rope2=new Rope(bob2.body,roof.body,-40, 0)
+	rope3=new Rope(bob3.body,roof.body,-0,0)
+	rope4=new Rope(bob4.body,roof.body,40,0)
+    rope5=new Rope(bob5.body,roof.body,80,0)
+	//roof= new Roof(200,200,200,20)
 	Engine.run(engine);
 	
   
@@ -38,7 +40,7 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(230);
-  roofObject.display();
+  //roofObject.display();
 
   rope1.display();
   rope2.display();
@@ -50,12 +52,12 @@ function draw() {
   bob3.display();
   bob4.display();
   bob5.display();
-
+ roof.display()
 }
 function keyPressed(){
 
 	if(keyCode === UP_ARROW){
-		Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-50,y:-45});
+		Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-50,y:-45});
 	}
 }
 
